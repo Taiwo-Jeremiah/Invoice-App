0ccc399c9f39bb05a51e9a5ef1dee4f17223a21e
Taiwo Jeremiah <taiwojeremiah1@gmail.com>
2026-04-24 00:43:41 +0100

    Fix: - Configure router to match the repository sub-path (/Invoice-App)

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Add this
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { InvoiceProvider } from "./context/InvoiceContext";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Add the basename right here! */}
     <BrowserRouter basename="/Invoice-App">
-      {" "}
-      {/* Wrap the Provider */}
-      <InvoiceProvider>
-        <App />
-      </InvoiceProvider>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
